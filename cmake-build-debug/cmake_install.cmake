861662935be9dc34a907ec96a9ef176a6eb6e02b
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-03 16:18:26 +0300

    c++ code, which works

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nastia/CLionProjects/at_class
+# Install script for directory: /home/nastia/CLionProjects/second_assignment
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nastia/CLionProjects/at_class/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nastia/CLionProjects/second_assignment/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 
 
-const int MAX_BUFFER_SIZE = 500;
+const int MAX_BUFFER_SIZE = 100;
 
 void Text_to_append(char*& Buffer, bool& has_text) {
     std::cout << "Enter text to append:";
@@ -13,39 +13,39 @@ void Text_to_append(char*& Buffer, bool& has_text) {
         Buffer = new char[strlen(text) + 1];
         strcpy(Buffer, text);
     } else {
-        char* temp = new char[strlen(Buffer) + strlen(text) + 2];
+        char* temp = new char[strlen(Buffer) + strlen(text) + 1];
         strcpy(temp, Buffer);
-        strcat(temp, "\n");
+        strcat(temp, " ");
         strcat(temp, text);
         delete[] Buffer;
         Buffer = temp;
     }
     has_text = true;
+    std::cout << Buffer;
 }
 
 void New_Line(char*& Buffer) {
-    char* temp = new char[strlen(Buffer) + 2];
-    strcpy(temp, Buffer);
-    strcat(temp, "\n");
-    delete[] Buffer;
-    Buffer = temp;
+    strcat(Buffer, "\n");
     std::cout << "New line is started\n";
 }
 
-void For_saving_in_file(const std::string& Buffer, std::string file_for_saving) {
-    std::cout << "Enter the file name for saving:";
-    std::ofstream file(file_for_saving.c_str());
+void For_saving_in_file(const std::string& Buffer) {
+    std::string file_for_saving;
+    std::cout << "Enter the file name for saving: ";
+    std::cin >> file_for_saving;
+
+    std::ofstream file(file_for_saving);
+
     if (file.is_open()) {
         file << Buffer;
         file.close();
-        std::cout << "Text has been saved successfully\n";
+        std::cout << "Text has been saved successfully" << std::endl;
     } else {
-        std::cout << "Error opening file for saving\n";
+        std::cerr << "Error: Unable to open file for writing" << std::endl;
     }
 }
 
-
-void For_loading_in_life(char* file_for_loading, char*& loaded_text) {
+void For_loading_in_file(char* file_for_loading, char*& loaded_text) {
     std::cout << "Enter the file name for loading:";
     std::cin >> file_for_loading;
     std::ifstream load_file(file_for_loading);
@@ -78,10 +78,10 @@ void Insert_an_index(int index1, int index2, const char* word_for_adding, char*&
     char* lines[20];
     int num_lines = 0;
     char* line = strtok(Buffer, "\n");
-    while (line != NULL) {
+    while (line != nullptr) {
         lines[num_lines] = line;
         num_lines++;
-        line = strtok(NULL, "\n");
+        line = strtok(nullptr, "\n");
     }
 
     if (index1 >= 0 && index1 < num_lines && index2 >= 0 && index2 <= strlen(lines[index1])) {
@@ -113,12 +113,12 @@ void Text_to_search(char*& Buffer, const char* text_to_search) {
     std::cin >> search_text;
     int row = 0;
     char* line = strtok(Buffer, "\n");
-    while (line != NULL) {
+    while (line != nullptr) {
         int position = 0;
         char* line_start = line;
         while (1) {
             char* word_in_line = strstr(line_start, search_text);
-            if (word_in_line != NULL) {
+            if (word_in_line != nullptr) {
                 position = word_in_line - line_start;
                 std::cout << "Text '" << search_text << "' found in line " << row << " at position " << position << std::endl;
                 line_start = word_in_line + 1;
@@ -127,12 +127,13 @@ void Text_to_search(char*& Buffer, const char* text_to_search) {
             }
         }
         row++;
-        line = strtok(NULL, "\n");
+        line = strtok(nullptr, "\n");
     }
     if (row == 0) {
         std::cout << "The word '" << search_text << "' is not found in the Buffer." << std::endl;
     }
 }
+
 
 int main() {
     int command;
@@ -141,10 +142,12 @@ int main() {
     char file_for_loading[MAX_BUFFER_SIZE];
     char word_for_adding[MAX_BUFFER_SIZE];
     char text_to_search[MAX_BUFFER_SIZE];
+    int index1, index2;
     char* loaded_text = nullptr;
     bool has_text = false;
 
-    while (true) {
+
+    while(true) {
         std::cout << "\nChoose the command!\n";
         std::cin >> command;
         std::cin.ignore();
@@ -156,16 +159,16 @@ int main() {
                 New_Line(Buffer);
                 break;
             case 3:
-                For_saving_in_file(Buffer, file_for_saving);
+                For_saving_in_file(Buffer);
                 break;
             case 4:
-                For_loading_in_life(file_for_loading, loaded_text);
+                For_loading_in_file(file_for_loading, loaded_text);
                 break;
             case 5:
                 Print_loading_text(loaded_text);
                 break;
             case 6:
-                Insert_an_index(0, 0, word_for_adding, Buffer);
+                Insert_an_index(index1, index2, word_for_adding, Buffer);
                 break;
             case 7:
                 Text_to_search(Buffer, text_to_search);
